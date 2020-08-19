04ca5281aa4747ba895bd460f58b098705e1011f
Krzysiek <chrisphilipniznik@gmail.com>
2020-08-19 20:22:53 +0200

    Implement HTTP Post request method in addStoryPanel.js, update respective files/classes linked

--- routes/addStoryPanel.js
+++ routes/addStoryPanel.js
@@ -10,22 +10,23 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-    var newData = [req.body.title_story, req.body.user_story, req.body.acceptance_criteria,
-        req.body.business_value, req.body.estimation];
+    var newData = [req.body.getUrl, req.body.getTitle, req.body.getDescription,
+        req.body.getTag, req.body.getUpload, req.body.getEmail];
     console.log(newData[0]);
     console.log(newData[1]);
     console.log(newData[2]);
     console.log(newData[3]);
     console.log(newData[4]);
+    console.log(newData[5]);
     writer = csvWriter({sendHeaders: false});
     writer.pipe(fs.createWriteStream('data.csv', {flags: 'a'}));
     writer.write({
         header1: newData[0],
         header2: newData[1],
         header3: newData[2],
-        header1: newData[3],
-        header2: newData[4],
-        header3: newData[5]
+        header4: newData[3],
+        header5: newData[4],
+        header6: newData[5]
     });
     writer.end();
     res.redirect("/showStories");
--- routes/showStories.js
+++ routes/showStories.js
@@ -7,7 +7,9 @@ let data = fs.readFileSync('data.csv')
     .toString() // convert Buffer to string
     .split('\n') // split string to lines
     .map(e => e.trim()) // remove white spaces for each line
-    .map(e => e.split(';').map(e => e.trim())); // split each line to array
+    .map(e => e.split(',').map(e => e.trim())); // split each line to array
+
+data.pop();
 
 let newData = [];
 
@@ -15,7 +17,7 @@ router.get('/', function (req, res, next) {
     /* GET users listing. */
     newData = shuffle(data).slice(0, 6);
     res.render('showStories', {data: newData});
-    console.log(data[0][5]);
+    console.log(data);
 });
 
 router.post('/', function (req, res, next) {
